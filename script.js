451c2c84d6513371d28954a45aca304918bcf9fb
Mehmet js <89641679+mehmetpy@users.noreply.github.com>
2022-08-21 03:37:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 setTimeout(function(){   
-    window.location.assign("https://mehmet-js.ml"); //We specify the page to be redirected
+    window.location.assign("https://mehmetwebs.ml"); //We specify the page to be redirected
     //Will Redirect After 5 Seconds
     }, 5000);//We specify how many seconds later we want it to be redirected
